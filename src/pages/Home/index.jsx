ab7626cda1ba3a6f02aa464bd44f61976844d801
Hexanima <nicolasfilippelli22@gmail.com>
2023-08-11 22:17:40 -0300

    Added details to home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,31 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import styles from "./home.module.css";
 
 function Home() {
   const infoText =
     "Este es el portafolio de Nicolás Filippelli, el cual aun está en desarrollo, a futuro habrá mejor información, muchas gracias.";
-  useEffect(() => {}, []);
+
+  const [text, setText] = useState("");
+  const [pos, setPos] = useState(0);
+  const [start, setStart] = useState(false);
+
+  useEffect(() => {
+    if (start && pos < infoText.length) {
+      const timeout = setTimeout(() => {
+        setText(prevText => prevText + infoText[pos])
+        setPos(prevPos => prevPos + 1)
+      }, 50)
+    } else {
+      const timeout = setTimeout(() => {
+        setStart(true)
+      }, 2500)
+    }
+  }, [pos, start]);
 
   return (
     <div className={styles.title}>
       <h1>¡Hola! ¿Qué tal?</h1>
-      <h5>{infoText}</h5>
+      <h5>{text}</h5>
     </div>
   );
 }
