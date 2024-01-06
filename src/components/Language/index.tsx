bfba9f01145c85d44c4978684b0a0d39617badcf
Hexanima <nicolasfilippelli22@gmail.com>
2024-01-06 13:45:13 -0300

    Added languages and modified styles

--- src/components/Language/index.tsx
+++ src/components/Language/index.tsx
@@ -4,7 +4,34 @@ import styles from "./language.module.scss";
 function Language() {
   return (
     <Container>
-      <Container.Title text="Idioma"/>
+      <Container.Title text="Idioma" />
+      <ul className={styles.Language}>
+        <li>
+          <h3>Ingles</h3>
+          <h4>Grado C2</h4>
+          <a href="">Certificado</a>
+        </li>
+        <li>
+          <h3>Ingles</h3>
+          <h4>Grado C2</h4>
+          <a href="">Certificado</a>
+        </li>
+        <li>
+          <h3>Ingles</h3>
+          <h4>Grado C2</h4>
+          <a href="">Certificado</a>
+        </li>
+        <li>
+          <h3>Ingles</h3>
+          <h4>Grado C2</h4>
+          <a href="">Certificado</a>
+        </li>
+        <li>
+          <h3>Ingles</h3>
+          <h4>Grado C2</h4>
+          <a href="">Certificado</a>
+        </li>
+      </ul>
     </Container>
   );
 }
