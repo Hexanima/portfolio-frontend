ae300e94778dc45f3a632ccac1703f6c9a4e556d
Hexanima <nicolasfilippelli22@gmail.com>
2024-01-16 23:12:28 -0300

    Added build compatibility

--- src/context/InfoProvider.tsx
+++ src/context/InfoProvider.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useState, useEffect } from "react";
 
 interface InfoContextValues {
   experienceList: ListInfo[];
@@ -10,79 +10,89 @@ interface InfoContextValues {
 export const InfoContext = createContext({} as InfoContextValues);
 
 export function InfoProvider({ children }: { children: ReactNode }) {
-  const [experienceList, setExperienceList] = useState<ListInfo[]>([
-    {
-      id: 1,
-      name: "Desarrollador Freelance",
-      location: "Le Baguette",
-      date: "11/23",
-      description: [
-        "Diseño y desarrollo de sitio web para panificadora en crecimiento.",
-      ],
-      liveUrl: "https://www.lebaguettepanificadora.com.ar",
-    },
-    {
-      id: 2,
-      name: "Panadero",
-      location: "Le Baguette",
-      date: "6/22 - Actualidad",
-      description: [
-        "Armado de pedidos para despacho a diferentes panaderías y despensas, procesando más de 250kg de pan por día.",
-        "Producción de diferentes panificados para venta y despacho, usando más de 300kg de harina al día.",
-      ],
-    },
-  ]);
+  const [experienceList, setExperienceList] = useState<ListInfo[]>([]);
 
-  const [educationList, setEducationList] = useState<ListInfo[]>([
-    {
-      id: 1,
-      name: "Desarrollador .NET",
-      description: [
-        "Curso autodidacta donde aprendo el lenguaje C# y el framework .NET",
-        "WPF, Databases, Linq, Collections, Game Development con Unity, Unit Testing TDD",
-      ],
-      date: "1/24 - Actualidad",
-      location: "Udemy - Tutorials.eu por Denis Panjuta",
-    },
-    {
-      id: 2,
-      name: "Desarrollador FullStack JS",
-      description: [
-        "Curso en el cual aprendimos desde el diseño hasta el desarrollo de un sitio web, y aplicamos estos conocimientos creando un ecommerce con vistas responsive, sistemas ABM, uso de cookies y creación de APIs.",
-      ],
-      date: "9/22 - 7/23",
-      location: "Digital House",
-    },
-  ]);
+  const [educationList, setEducationList] = useState<ListInfo[]>([]);
 
-  const [languageList, setLanguageList] = useState<LanguageInfo[]>([
-    { id: 1, title: "Español", grade: "Nativo" },
-    {
-      id: 2,
-      title: "Ingles",
-      grade: "Grado C2",
-      certificate: "https://www.efset.org/cert/hDaGDF",
-    },
-  ]);
+  const [languageList, setLanguageList] = useState<LanguageInfo[]>([]);
 
-  const [projectList, setProjectList] = useState<ProjectInfo[]>([
-    {
-      id: 1,
-      img: "img/CloneTheSpire.png",
-      title: "Clone the Spire",
-      info: `Proyecto en donde clono las mecanicas y jugabilidad del videojuego
-  "Slay The Spire" en una aplicacion de consola, a fin de aprender el lenguaje C#`,
-      gitUrl: "https://github.com/Hexanima/clone-the-spire",
-    },
-    {
-      id: 2,
-      img: "img/AnimalPark.png",
-      title: "Animal Park",
-      info:
-        "Proyecto integrador para el curso de Desarrollador Web Full Stack dado por la Fundación Formar junto a Digital House.",
-      gitUrl: "https://github.com/RamonBritez/Grupo_5_Animal_Park",
-    },
-  ]);
+  const [projectList, setProjectList] = useState<ProjectInfo[]>([]);
+
+  useEffect(() => {
+    setExperienceList([
+      {
+        id: 1,
+        name: "Desarrollador Freelance",
+        location: "Le Baguette",
+        date: "11/23",
+        description: [
+          "Diseño y desarrollo de sitio web para panificadora en crecimiento.",
+        ],
+        liveUrl: "https://www.lebaguettepanificadora.com.ar",
+      },
+      {
+        id: 2,
+        name: "Panadero",
+        location: "Le Baguette",
+        date: "6/22 - Actualidad",
+        description: [
+          "Armado de pedidos para despacho a diferentes panaderías y despensas, procesando más de 250kg de pan por día.",
+          "Producción de diferentes panificados para venta y despacho, usando más de 300kg de harina al día.",
+        ],
+      },
+    ]);
+
+    setEducationList([
+      {
+        id: 1,
+        name: "Desarrollador .NET",
+        description: [
+          "Curso autodidacta donde aprendo el lenguaje C# y el framework .NET",
+          "WPF, Databases, Linq, Collections, Game Development con Unity, Unit Testing TDD",
+        ],
+        date: "1/24 - Actualidad",
+        location: "Udemy - Tutorials.eu por Denis Panjuta",
+      },
+      {
+        id: 2,
+        name: "Desarrollador FullStack JS",
+        description: [
+          "Curso en el cual aprendimos desde el diseño hasta el desarrollo de un sitio web, y aplicamos estos conocimientos creando un ecommerce con vistas responsive, sistemas ABM, uso de cookies y creación de APIs.",
+        ],
+        date: "9/22 - 7/23",
+        location: "Digital House",
+      },
+    ]);
+
+    setLanguageList([
+      { id: 1, title: "Español", grade: "Nativo" },
+      {
+        id: 2,
+        title: "Ingles",
+        grade: "Grado C2",
+        certificate: "https://www.efset.org/cert/hDaGDF",
+      },
+    ]);
+
+    setProjectList([
+      {
+        id: 1,
+        img: "img/CloneTheSpire.png",
+        title: "Clone the Spire",
+        info: `Proyecto en donde clono las mecanicas y jugabilidad del videojuego
+    "Slay The Spire" en una aplicacion de consola, a fin de aprender el lenguaje C#`,
+        gitUrl: "https://github.com/Hexanima/clone-the-spire",
+      },
+      {
+        id: 2,
+        img: "img/AnimalPark.png",
+        title: "Animal Park",
+        info:
+          "Proyecto integrador para el curso de Desarrollador Web Full Stack dado por la Fundación Formar junto a Digital House.",
+        gitUrl: "https://github.com/RamonBritez/Grupo_5_Animal_Park",
+      },
+    ]);
+  }, []);
 
   const contextValues = {
     experienceList,
--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 import styles from "./layout.module.scss";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
+//import Footer from "./components/Footer";
 
 function Layout({ children }: { children: ReactNode }) {
   return (
