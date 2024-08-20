97bb7e95fbafeaef1d1fb71a3a8eac2cebf91ef3
Nicolas Filippelli <nicolasfilippelli22@gmail.com>
2024-08-20 18:21:54 -0300

    Fixed page loading before images

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,11 +1,13 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import styles from "./layout.module.scss";
 import Header from "./components/Header";
 //import Footer from "./components/Footer";
 
 function Layout({ children }: { children: ReactNode }) {
+  const [readyToShow, setReadyToShow] = useState(false)
+
   return (
-    <div className={styles.Layout}>
+    <div className={`${styles.Layout} ${readyToShow && styles.Loaded}`} onLoad={() => setReadyToShow(true)}>
       <Header />
       <main>{children}</main>
       {/* <Footer /> */}
