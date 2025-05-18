532b0366d4630b705b742fc34fd23829c712c81a
Nicolas Filippelli <nicolasfilippelli22@gmail.com>
2025-05-18 11:28:31 -0300

    Fixed Banner styles

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -7,8 +7,8 @@ function Banner() {
       <div className={styles.Info}>
         <img src="/img/Profile Picture.jpeg" alt="" />
         <div className={styles.Data}>
-          <h1>NICOLAS FILIPPELLI</h1>
-          <h2>FULLSTACK DEVELOPER</h2>
+          <h1>Nicolas Filippelli</h1>
+          <h2>Fullstack Developer</h2>
         </div>
       </div>
     </div>
